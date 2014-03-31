246e4f51703fa26769ec8a6f80cc2824597210f1
Velesey <vladimir9292@list.ru>
2014-03-31 08:14:57 +0600

    анимация игрока. Убрал внешнюю библиотеку

--- player.py
+++ player.py
@@ -2,17 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from pygame import *
-import pyganim
 import os
 
-MOVE_SPEED = 10
 WIDTH = 32
 HEIGHT = 32
-COLOR =  "#888888"
-ANIMATION_DELAY = 0.5 # скорость смены кадров
-ICON_DIR = os.path.dirname(__file__) #  Полный путь к каталогу с файлами
+ICON_DIR = os.path.dirname(__file__)
 
-ANIMATION_RIGHT = [('%s/images/hero_1.png' % ICON_DIR),
+ANIMATION = [('%s/images/hero_1.png' % ICON_DIR),
                    ('%s/images/hero_2.png' % ICON_DIR)]
 
 class Player(sprite.Sprite):
@@ -20,29 +16,28 @@ class Player(sprite.Sprite):
         sprite.Sprite.__init__(self)
         self.image = Surface((WIDTH,HEIGHT))
         self.rect = Rect(x, y, WIDTH, HEIGHT)
-        self.image.set_colorkey(Color(COLOR))
-        boltAnim = []
-        for anim in ANIMATION_RIGHT:
-            boltAnim.append((anim, ANIMATION_DELAY))
-        self.boltAnimRight = pyganim.PygAnimation(boltAnim)
-        self.boltAnimRight.play()
         self.winner = False
+        self.animationState  = 0
         
-
     def update(self, speed, finish):
-        self.image.fill(Color(COLOR))
-        self.boltAnimRight.blit(self.image, (0, 0))
+        if speed > 0:
+            self.animationState+=1;
+            if self.animationState == 2: self.animationState = 0
+        self.image = image.load(ANIMATION[self.animationState])
         self.rect.x += speed
         if sprite.collide_rect(self, finish):
             self.winner = True
 
-
 class LastPlayer(Player):
     def __init__(self,x,y):
         Player.__init__(self,x,y)
+        self.lastDistance = 0
 
     def update(self, distance):
-        self.image.fill(Color(COLOR))
-        self.boltAnimRight.blit(self.image, (0, 0))
+        if self.lastDistance != distance:
+            self.animationState+=1;
+            if self.animationState == 2: self.animationState = 0
+        self.lastDistance = distance
+        self.image = image.load(ANIMATION[self.animationState])
         self.rect.x = distance
 
